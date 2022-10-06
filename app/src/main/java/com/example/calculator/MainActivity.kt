b242109ce5e057a423435aaf8804dc797fce8da3
Akeem <akeem.mighty@gmail.com>
2022-10-05 20:40:35 -0400

    Added Expression Builder Library + Changed Number and Operator From TextView To Buttons + Toggling onClick Event Listener in activity_main.xml File

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.example.calculator
 
 import android.os.Bundle
-//import android.widget.TextView
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-//import net.objecthunter.exp4j.ExpressionBuilder
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+    lateinit var txt:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txt = findViewById(R.id.calcExpression)
+//        res = findViewById(R.id.calcResult)
     }
+    fun onNum(view: View)
+    {
+        txt.append((view as Button).text)
+    }
+    fun onOperator(view: View)
+    {
+        txt.append((view as Button).text)
+    }
+    fun onDot(view: View)
+    {
+        txt.append((view as Button).text)
+    }
+    fun onDel(view: View)
+    {
+        this.txt.text =""
+    }
+    fun onClear(view: View)
+    {
+        this.txt.text =""
+    }
+    fun onEquals(view: View)
+    {
+        val text = txt.text.toString()
+        val eval = ExpressionBuilder(text).build()
+        val res = eval.evaluate()
+        txt.text = res.toString()
+    }
+
 
 }
