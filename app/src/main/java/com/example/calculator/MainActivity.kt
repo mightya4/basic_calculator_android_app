0f1d275b8eb34cf83d028548c5d69f0a8f6d5c6d
Akeem <akeem.mighty@gmail.com>
2022-10-08 21:31:45 -0400

    Completed Number onclick Functionality in MainActivity.kt File + Completed Action onclick Functionality in MainActivity.kt File + Removed Unecessary Colors From Colors.xml + Added Kotlin-android-extensions plugin in app build.gradle File + Updated TextView Height Attribute Values in activity_main.xml File + Removed TextView Onclick Function in activity_main.xml File

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,47 +1,101 @@
 package com.example.calculator
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var txt:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txt = findViewById(R.id.calcExpression)
-//        res = findViewById(R.id.calcResult)
-    }
-    fun onNum(view: View)
-    {
-        txt.append((view as Button).text)
-    }
-    fun onOperator(view: View)
-    {
-        txt.append((view as Button).text)
-    }
-    fun onDot(view: View)
-    {
-        txt.append((view as Button).text)
-    }
-    fun onDel()
-    {
-        this.txt.text =""
-    }
-    fun onClear(view: View)
-    {
-        this.txt.text =""
-    }
-    fun onEquals()
-    {
-        val text = txt.text.toString()
-        val eval = ExpressionBuilder(text).build()
-        val res = eval.evaluate()
-        txt.text = res.toString()
+
+        /* Number events */
+        calcOne.setOnClickListener {
+            evaluateExpression("1", clear = true)
+        }
+        calcTwo.setOnClickListener {
+            evaluateExpression("2", clear = true)
+        }
+        calcThree.setOnClickListener {
+            evaluateExpression("3", clear = true)
+        }
+        calcFour.setOnClickListener {
+            evaluateExpression("4", clear = true)
+        }
+        calcFive.setOnClickListener {
+            evaluateExpression("5", clear = true)
+        }
+        calcSix.setOnClickListener {
+            evaluateExpression("6", clear = true)
+        }
+        calcSeven.setOnClickListener {
+            evaluateExpression("7", clear = true)
+        }
+        calcEight.setOnClickListener {
+            evaluateExpression("8", clear = true)
+        }
+        calcNine.setOnClickListener {
+            evaluateExpression("9", clear = true)
+        }
+        calcZero.setOnClickListener {
+            evaluateExpression("0", clear = true)
+        }
+
+
+        /* Operator events */
+        calcPlus.setOnClickListener {
+            evaluateExpression("+", clear = true)
+        }
+        calcMinus.setOnClickListener {
+            evaluateExpression("-", clear = true)
+        }
+        calcMul.setOnClickListener {
+            evaluateExpression("*", clear = true)
+        }
+        calcDivide.setOnClickListener {
+            evaluateExpression("/", clear = true)
+        }
+        calcDot.setOnClickListener {
+            evaluateExpression(".", clear = true)
+        }
+
+        /* Action events */
+        calcClear.setOnClickListener {
+            calcExpression.text = ""
+            calcResult.text = ""
+        }
+        calcEquals.setOnClickListener {
+            val text = calcExpression.text.toString()
+            val expression = ExpressionBuilder(text).build()
+
+            val result = expression.evaluate()
+            val longResult = result.toLong()
+            if ( result == longResult.toDouble() ) {
+                calcResult.text = longResult.toString()
+            } else {
+                calcResult.text = result.toString()
+            }
+        }
+        calcBack.setOnClickListener {
+            val text = calcExpression.text.toString()
+            if(text.isNotEmpty()) {
+                calcExpression.text = text.drop(1)
+            }
+            calcResult.text = ""
+        }
     }
 
+    private fun evaluateExpression(s: String, clear: Boolean) {
+        if( clear ) {
+            calcResult.text = ""
+            calcExpression.append(s)
+        } else {
+            calcExpression.append(calcResult.text)
+            calcExpression.append(s)
+            calcResult.text = ""
+        }
+    }
 
 }
